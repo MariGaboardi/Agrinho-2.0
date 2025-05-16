943796d207f908497fefb60f4fbc1630c98fd06e
MariGaboardi <160857358+MariGaboardi@users.noreply.github.com>
2025-05-16 07:55:55 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,68 @@
+criaCartao(
+    'Para dias de insegurança',
+    'Colossenses 3:23',
+    'Tudo o que fizerdes, fazei-o de bom coração, com para o Senhor e não para os homens.'
+)
+
+criaCartao(
+    ' Quesito Geográfico',
+    'Qual a cidade em que Jesus nasceu?',
+    'Jesus nasceu em Belém, uma cidade atualmente localizada no território palestino da Cisjordânia, cerca de 10 quilômetros ao sul de Jerusalém.'
+)
+
+criaCartao(
+    'Para dias de peocupações',
+    'Provérbios 16:3',
+    'Confia teus negócios ao Senhor e teus planos terão bom exito.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se escreve jesus Cristo em inglês?',
+    'Jesus Christ'
+)
+
+criaCartao(
+    'Para dias de esgotamento físico e psicológico',
+    'Mateus 11:28',
+    'Vinde a mim, vós todos que estais aflitos sob o fardo, e eu vos aliviarei.'
+)
+
+
+criaCartao(
+    'Pare de relamar e entenda isso!!',
+    '1 Tessalonicenses 5:18',
+    'Em todas circunstâncias, dai graça, porque esta é a vosso respeito a vontade de Deus em Jesus Cristo.'
+)que fizerdes, fazei-o de bom coração, com para o Senhor e não para os homens.'
+)
+
+criaCartao(
+    ' Quesito Geográfico',
+    'Qual a cidade em que Jesus nasceu?',
+    'Jesus nasceu em Belém, uma cidade atualmente localizada no território palestino da Cisjordânia, cerca de 10 quilômetros ao sul de Jerusalém.'
+)
+
+criaCartao(
+    'Para dias de peocupações',
+    'Provérbios 16:3',
+    'Confia teus negócios ao Senhor e teus planos terão bom exito.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se escreve jesus Cristo em inglês?',
+    'Jesus Christ'
+)
+
+criaCartao(
+    'Para dias de esgotamento físico e psicológico',
+    'Mateus 11:28',
+    'Vinde a mim, vós todos que estais aflitos sob o fardo, e eu vos aliviarei.'
+)
+
+
+criaCartao(
+    'Pare de relamar e entenda isso!!',
+    '1 Tessalonicenses 5:18',
+    'Em todas circunstâncias, dai graça, porque esta é a vosso respeito a vontade de Deus em Jesus Cristo.'
+)
