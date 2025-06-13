5141b1db65653e2bffe795656c814fc96f189c0f
MariGaboardi <160857358+MariGaboardi@users.noreply.github.com>
2025-06-13 07:33:19 -0300

    sad

--- perguntas.js
+++ perguntas.js
@@ -1,67 +1,36 @@
-    'Vantagem - cidade',
-    'Colossenses 3:23',
-    'Tudo o que fizerdes, fazei-o de bom coração, com para o Senhor e não para os homens.'
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
 )
 
 criaCartao(
-    ' Desvantagem - cidade',
-    'Qual a cidade em que Jesus nasceu?',
-    'Jesus nasceu em Belém, uma cidade atualmente localizada no território palestino da Cisjordânia, cerca de 10 quilômetros ao sul de Jerusalém.'
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
 )
 
 criaCartao(
-    'Vantagem - cidade',
-    'Provérbios 16:3',
-    'Confia teus negócios ao Senhor e teus planos terão bom exito.'
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-    'Vantagem - campo',
-    'Como se escreve jesus Cristo em inglês?',
-    'Jesus Christ'
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
 )
 
 criaCartao(
-    'Desvantagem - campo',
-    'Mateus 11:28',
-    'Vinde a mim, vós todos que estais aflitos sob o fardo, e eu vos aliviarei.'
-)
-
-
-criaCartao(
-    'Vantagem - campo',
-    '1 Tessalonicenses 5:18',
-    'Em todas circunstâncias, dai graça, porque esta é a vosso respeito a vontade de Deus em Jesus Cristo.'
-)que fizerdes, fazei-o de bom coração, com para o Senhor e não para os homens.'
-)
-
-criaCartao(
-    ' Vantagem - cidade',
-    'Qual a cidade em que Jesus nasceu?',
-    'Jesus nasceu em Belém, uma cidade atualmente localizada no território palestino da Cisjordânia, cerca de 10 quilômetros ao sul de Jerusalém.'
-)
-
-criaCartao(
-    'Desvantagem - cidade',
-    'Provérbios 16:3',
-    'Confia teus negócios ao Senhor e teus planos terão bom exito.'
-)
-
-criaCartao(
-    'Vantagem - cidade',
-    'Como se escreve jesus Cristo em inglês?',
-    'Jesus Christ'
-)
-
-criaCartao(
-    'Vantagem - campo',
-    'Mateus 11:28',
-    'Vinde a mim, vós todos que estais aflitos sob o fardo, e eu vos aliviarei.'
+    'Programação',
+    'Qual a finalidade da variável em javascript?',
+    'Armazenar valores aleatórios a serem transcritos no html'
 )
 
 
 criaCartao(
-    'Vantagem - campo!!',
-    '1 Tessalonicenses 5:18',
-    'Em todas circunstâncias, dai graça, porque esta é a vosso respeito a vontade de Deus em Jesus Cristo.'
+    'Programação',
+    'Qual a finalidade da variável em javascript?',
+    'Armazenar valores aleatórios a serem transcritos no html'
 )
